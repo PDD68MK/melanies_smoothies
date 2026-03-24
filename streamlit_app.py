01967920ec0ddcc9e8620497950de5b693d3843d
PDD68MK <paul.davies.mk@gmail.com>
2026-03-24 14:10:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
+import pandas
 
 cnx = st.connection("snowflake")
 session = cnx.session() 
@@ -14,9 +15,10 @@ st.write(
 name_on_order = st.text_input('Name on order')
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop
-                                                                                             
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop
+pd_df = my_dataframe.to_pandas()
+
 ingredients_list = st.multiselect('Choose up to five ingredients: ', my_dataframe, max_selections = 5)
 
 if ingredients_list:
