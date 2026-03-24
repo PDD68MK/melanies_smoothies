c8fa3af2521df6224017af208055dfba68d8dd1d
PDD68MK <paul.davies.mk@gmail.com>
2026-03-24 10:01:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session() 
+#get_active_session()
 
 
 # Write directly to the app
