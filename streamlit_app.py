16750c97126cd0dadaadde946e83e6aceb50923e
PDD68MK <paul.davies.mk@gmail.com>
2026-03-24 12:26:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
 name_on_order = st.text_input('Name on order')
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe. use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop
                                                                                              
 ingredients_list = st.multiselect('Choose up to five ingredients: ', my_dataframe, max_selections = 5)
